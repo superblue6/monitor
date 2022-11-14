b8706f83ec4c8459f6f5efd81bc703cb9d33f83b
wangyutian <716164682@qq.com>
2022-11-14 15:07:39 +0800

    Initial commit

---
+++ src/test/java/com/wang/monitor/MonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
